1ff5a1e901b402f4e633c85bb2f9a43ce6a8c3fd
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-10-14 20:24:48 -0300

    style: rename App.css to styles.module.css and add header styling with updated imports in index.tsx

--- jogo_de_adivinhacao/src/App.tsx
+++ jogo_de_adivinhacao/src/App.tsx
@@ -1,7 +1,10 @@
+import styles from "./app.module.css"
+import { Header } from './components/Header'
+
 export default function App() {
   return (
-    <div>
-      <h1> Jogo de Adivinhação </h1> 
+    <div className={styles.container}>
+      <Header /> 
     </div>
   )
 }
