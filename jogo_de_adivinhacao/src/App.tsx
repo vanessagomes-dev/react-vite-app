24cf927d3bc7c857513e994877600f4511376c52
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-10-15 11:21:51 -0300

    feat: create Dica component with dedicated styling for displaying contextual tips

--- jogo_de_adivinhacao/src/App.tsx
+++ jogo_de_adivinhacao/src/App.tsx
@@ -1,5 +1,8 @@
 import styles from "./app.module.css"
+
+import { Tip } from './components/Tip'
 import { Header } from './components/Header'
+
 
 export default function App() {
 
@@ -9,6 +12,7 @@ export default function App() {
   return (
     <div className={styles.container}>
       <Header current={5} max={10} onRestart={handleRestartGame}/> 
+      <Tip tip= "Uma das linguagens de programação mais utilizadas" />
     </div>
   )
 }
