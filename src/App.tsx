0d5e11a96b11f161b35d37dddc8f3474d426dc4c
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-10-17 16:56:24 -0300

    feat: create Input component for user guesses with styles and import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styles from "./app.module.css";
 
+import { Input } from "./components/Input";
 import { Tip } from "./components/Tip";
 import { Letter } from "./components/Letter";
 import { Header } from "./components/Header";
@@ -14,12 +15,18 @@ export default function App() {
         <Header current={5} max={10} onRestart={handleRestartGame} />
 
         <Tip tip="Uma das linguagens de programação mais utilizadas" />
-<div className={styles.word}>
-        <Letter value="R" />
-        <Letter value="E" />
-        <Letter value="A" />
-        <Letter value="C" />
-        <Letter value="T" />
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
+
+        <h4>Palpite</h4>
+
+        <div>
+          <Input autoFocus maxLength={1} placeholder="?" />
         </div>
       </main>
     </div>
