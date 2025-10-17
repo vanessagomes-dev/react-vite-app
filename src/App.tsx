fb216c92114f5471829280a238c878e0960ac72f
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-10-17 20:17:09 -0300

    feat: implement logic to check user guesses and validate correct letters

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { Header } from "./components/Header";
 import { LettersUsed, type LettersUsedProps } from "./components/LettersUsed";
 
 export default function App() {
+  const [score, setScore] = useState(0);
   const [letter, setLetter] = useState("");
   const [attempts, setAttempts] = useState(0);
   const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([]);
@@ -45,9 +46,17 @@ export default function App() {
       return alert("Você já tentou essa letra" + value);
     }
 
-    setLettersUsed((prevState) => [
-      ...prevState, { value, correct: false }])
+    const hits = challenge.word.toUpperCase()
+    .split("")
+    .filter((char) => char === value).length
 
+    const correct = hits > 0
+    const currentScore = score + hits
+
+
+    setLettersUsed((prevState) => [
+      ...prevState, { value, correct }])
+    setScore(currentScore);
     setLetter("");
       
   }
@@ -65,7 +74,7 @@ export default function App() {
       <main>
         <Header current={attempts} max={10} onRestart={handleRestartGame} />
 
-        <Tip tip="Uma das linguagens de programação mais utilizadas" />
+        <Tip tip={challenge.tip} />
         <div className={styles.word}>
           {challenge.word.split("").map(() => (
             <Letter value="" />
