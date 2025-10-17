812ec43a8aaf055295a13d3a470a0d638e7c2f68
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-10-17 18:46:06 -0300

    feat: implement function to randomly select words

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import styles from "./app.module.css";
 
+import { useEffect, useState } from "react";
+import { WORDS, type Challenge } from "./utils/words";
 import { Input } from "./components/Input";
 import { Button } from "./components/Button";
 import { Tip } from "./components/Tip";
@@ -7,14 +9,33 @@ import { Letter } from "./components/Letter";
 import { Header } from "./components/Header";
 import { LettersUsed } from "./components/LettersUsed";
 
+
 export default function App() {
+  const [attempts, setAttempts] = useState(0);
+  const [letter, setLetter] = useState("");
+  const [challenge, setChallenge] = useState<Challenge | null>(null);
+
   function handleRestartGame() {
-    alert("Reiniciar jogo!");
   }
+
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length);
+    const randomWord = WORDS[index];
+
+    setChallenge(randomWord);
+
+    setAttempts(0);
+    setLetter("");
+  }
+
+  useEffect(() => {
+    startGame();
+  }, []);
+
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Header current={attempts} max={10} onRestart={handleRestartGame} />
 
         <Tip tip="Uma das linguagens de programação mais utilizadas" />
         <div className={styles.word}>
